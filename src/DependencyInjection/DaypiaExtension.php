6fa906e447e8656ad0d4359b885b025a9b55e98d
Thomas Beaujean <thomas.beaujean@dayploy.fr>
2024-12-23 11:52:21 +0100

    typo un namespace

--- src/DependencyInjection/DaypiaExtension.php
+++ src/DependencyInjection/DaypiaExtension.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Dayploy\DaypliaBundle\DependencyInjection;
+namespace Dayploy\DaypiaBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
